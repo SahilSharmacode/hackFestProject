d8cdcffcfcfb684ce20ec14c4f871cf747c4baad
unknown <admin@31D-LAB3-15.SVV.local>
2023-12-14 04:39:17 +0530

    every this about user auth is done
    not the other part i will work on

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_web_app/screens/Verification_screen.dart';
 import 'package:my_web_app/screens/home_screen.dart';
 import 'package:my_web_app/screens/login_screen.dart';
+import 'package:my_web_app/screens/signup_screen.dart';
 import 'package:my_web_app/widgets/layout_builder.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -28,6 +37,15 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const LoginScreen();
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+         return VerificationScreen();
+        }else{
+          return LoginScreen();
+        }
+      },
+    );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_web_app/utils/themes.dart';
 
 
 double getScreenWidth(BuildContext context){
@@ -8,4 +9,57 @@ double getScreenWidth(BuildContext context){
 
 double getScreenHeight(BuildContext context){
   return MediaQuery.of(context).size.width;
+}
+
+
+void errorMessage(String message, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Container(
+          height: 40,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: Colors.red,
+          ),
+          child: Center(
+              child: Text(
+            message,
+            style: TextStyle(fontWeight: FontWeight.w400),
+          ))),
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    ),
+  );
+}
+
+void sucessMessage(String message, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Container(
+          height: 40,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: green,
+          ),
+          child: Center(
+              child: Text(
+            message,
+            style: TextStyle(fontWeight: FontWeight.w400),
+          ))),
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    ),
+  );
+}
+
+
+Widget loding(){
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 5),
+    child: AspectRatio(
+      aspectRatio: 1/1,
+      child: CircularProgressIndicator(color: white,)),
+  );
 }
